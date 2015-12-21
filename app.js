5ca5948cd0dd46235857d01eda4424b3312a3d4f
Maxine Fonua <maxinefonua@gmail.com>
2015-12-20 21:13:45 -0800

    cross domain works + database updates delete

--- app.js
+++ app.js
@@ -29,8 +29,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // Make our db accessible to our router
 app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, safedelete"); 
     req.db = db;
     next();
 });
@@ -47,6 +51,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
 
 // error handlers
 
