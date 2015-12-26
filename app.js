174c1de5832a2905df3556b712c409859ec42767
Shola Oyedele <sjoyedele@gmail.com>
2015-12-25 20:48:35 -0800

    Updated Access Control to allow user params to be passed in the headers.

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*"); 
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, safedelete"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, safedelete, email, username, profession, name"); 
     req.db = db;
     next();
 });
