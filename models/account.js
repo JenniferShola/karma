438b18951737baf90bb6efed0793f398f744a6ff
Shola Oyedele <sjoyedele@gmail.com>
2016-01-29 19:10:39 -0800

    Added bcrypt to encrypt account passwords.

--- models/account.js
+++ models/account.js
@@ -1,12 +1,13 @@
 var mongoose = require('mongoose');
 var config = require('config')
 var db = require('./cached');
+var bcrypt = require('bcrypt');
+
 
 var Schema = mongoose.Schema;
-var Types = Schema.Types;
 
-var properties = { 
-	_creator:           { type: String, ref: 'users', required: true, unique: false},
+var properties = {
+    _creator:           { type: String, ref: 'users', required: false, unique: false},
     username:           {type: String, required: true, unique: true},
     password:           {type: String, required: true, unique: false},
     signup:             {type: Number, required: false, default: Date.now},
@@ -14,6 +15,36 @@ var properties = {
 };
 
 var definition = new Schema(properties);
+
+definition.pre('save', function (next) {
+    var account = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(account.password, salt, function (err, hash) {
+                if (err) {
+                    return next(err);
+                }
+                account.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+definition.methods.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
+
 var table = config.get('mongo.dbtable');
 
 var model = db(table).model('accounts', definition);
